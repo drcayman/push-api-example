bd14f68a5ed1f9781ee0e143d3bcd64ccceee4b3
PascalAOMS <email@artofmyself.com>
2016-11-07 10:43:59 +0100

    toned down main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,9 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import esPolyfill from 'es-dynamic-polyfill'
-import App from './App.vue'
-import test from './test'
+import Polyfill from 'dynamic-polyfill'
 
-test()
-
-Vue.use(VueRouter)
-
-const routes = [
-    { path: '/', component: App }
-]
-
-let router = new VueRouter({
-    routes
-})
-
-
-const app = new Vue({
-    el: '#content',
-    router
-})
-
-
-esPolyfill({
+Polyfill({
     fills: 'fetch, promise',
     options: 'gated',
-    afterFill() { 
+    afterFill() {
         main()
     }
 })
@@ -34,23 +11,6 @@ esPolyfill({
 
 function main() {
 
-    console.log('hit ye!');
-
-    // document.getElementById('link').addEventListener('click', function() {
-    //
-    //     fetch('/about.html').then(res => res.text())
-    //     .then(res => {
-    //         //document.getElementById('content').innerHTML = res;
-    //         document.getElementById('content').innerHTML = res;
-    //
-    //         console.log(res);
-    //         alert(`Features: ${res}!`)
-    //
-    //     })
-    //     .catch(err => console.log(err));
-    //
-    // });
-
-
+    
 
 }
