662f1a33d70565212857831958c614008ea73633
PascalAOMS <email@artofmyself.com>
2017-02-19 22:00:20 +0100

    add blade boilerplate files

---
+++ src/views_laravel/layout/footer.blade.php
@@ -0,0 +1,11 @@
+
+
+<div>Footer</div>
+
+<script src="js/manifest.js"></script>
+<script src="js/vendor.js"></script>
+<script src="js/main.js"></script>
+
+
+</body>
+</html>
---
+++ src/views_laravel/layout/master.blade.php
@@ -0,0 +1,12 @@
+
+
+@include('layout.header')
+
+
+@include('layout.nav')
+
+
+@yield('content')
+
+
+@include('layout.footer')
